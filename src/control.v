750365579b857d24653dca597d808bcbb7875e4d
Aly Youssef <alyhassan2254@gmail.com>
2025-10-21 18:51:40 +0300

    added comments and other types of instructions

--- src/control.v
+++ src/control.v
@@ -5,9 +5,10 @@ module control(
 input [31:0] instr, output reg branch, memRead, MemtoReg, MemWrite, ALUSrc, RegWrite, output reg [1:0] ALUOp 
     );
 wire [4:0] specifier = instr[6:2];
-
+// specifier is the last 5 bits of the instruction
 always@(*) begin
     case(specifier)
+        // R-type instructions
         5'b01100: begin
         assign branch = 1'b0;
         memRead = 1'b0;
@@ -17,6 +18,7 @@ always@(*) begin
         ALUSrc = 1'b0;
         RegWrite = 1'b1;
         end
+        // Load instructions
         5'b00000: begin
         branch = 1'b0;
         memRead = 1'b1;
@@ -26,6 +28,7 @@ always@(*) begin
         ALUSrc = 1'b1;
         RegWrite = 1'b1;
         end
+        // store instructions
         5'b01000: begin
         branch = 1'b0;
         memRead = 1'b0;
@@ -35,14 +38,25 @@ always@(*) begin
         ALUSrc = 1'b1;
         RegWrite = 1'b0;
         end
+        // branch instructions
         5'b11000: begin
         branch = 1'b1;
         memRead = 1'b0;
         MemtoReg = 1'bx;
         ALUOp = 2'b01;
         MemWrite = 1'b0;
-        ALUSrc = 1'b0;
+        ALUSrc = 1'b1;
         RegWrite = 1'b0;
+        end
+        // I-type instructions other than Load
+        5'b00100: begin
+        branch = 1'b0;
+        memRead = 1'b0;
+        MemtoReg = 1'b0;
+        ALUOp = 2'b00; // not sure about this
+        MemWrite = 1'b0;
+        ALUSrc = 1'b1;
+        RegWrite = 1'b1;
         end
         default: begin
         branch = 1'bx;
